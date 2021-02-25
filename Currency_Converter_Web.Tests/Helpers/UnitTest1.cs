a0586126580610122af9782d89d2c9c2fc29e9e0
Alberto Sierra <lokdok1988@hotmail.com>
2021-02-25 19:25:08 +0100

    test

--- Currency_Converter_Web.Tests/Helpers/UnitTest1.cs
+++ Currency_Converter_Web.Tests/Helpers/UnitTest1.cs
@@ -12,7 +12,10 @@ namespace Currency_Converter_Web.Helpers.Tests
         [TestMethod()]
         public void CurrrencySelectedTotalTest()
         {
+            var currencySelected = new CurrencySelected();
+            double total = currencySelected.CurrrencySelectedTotal(1, 1.5);
 
+            Assert.AreEqual(1.5, total);
         }
     }
 }
